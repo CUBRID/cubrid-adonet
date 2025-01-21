6c3717c21ff7b59d3983a40ae2c71d2616e8c119
Kang-dot <jgbu97@gmail.com>
2025-01-21 15:41:40 +0900

    [APIS-1013] add print result to file function
    - add "-p" option for print result file

--- test.bat
+++ test.bat
@@ -5,4 +5,9 @@ call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd Code\
 devenv CUBRID.Data.sln /Project ADOTest /Rebuild "Release|x64" 
 cd Test\QATest\ADOTest\bin\Release
-call "%VSTestTool%" ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
+
+if "%1"=="-p" (
+  powershell -Command "& '%VSTestTool%' ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings | Tee-Object -FilePath '%BATCH_HOME%..\ado_net_test.result'"
+) else (
+  call "%VSTestTool%" ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
+)
