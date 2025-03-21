bd448d706bd02249b5a837c95f13ea1dda28f5d6
KangBu Jeong <54855268+Kang-dot@users.noreply.github.com>
2025-03-21 17:35:19 +0900

    [APIS-1013] edit ado.net test batch (#42)
    - fix directory location
    - add Functional test case in batch script

--- test.bat
+++ test.bat
@@ -4,10 +4,17 @@ set VSTestTool=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vst
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd Code\
 devenv CUBRID.Data.sln /Project ADOTest /Rebuild "Release|x64" 
-cd Test\QATest\ADOTest\bin\Release
 
 if "%1"=="-p" (
-  powershell -Command "& '%VSTestTool%' ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings | Tee-Object -FilePath '%BATCH_HOME%..\ado_net_test.result'"
+  powershell -Command "& '%VSTestTool%' .\Test\QATest\ADOTest\bin\Release\ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings | Tee-Object -FilePath '%BATCH_HOME%..\ado_net_test.result'"
 ) else (
-  call "%VSTestTool%" ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
+  call "%VSTestTool%" .\Test\QATest\ADOTest\bin\Release\ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
+)
+
+devenv CUBRID.Data.sln /Project Test.Functional /Rebuild "Release|x64"
+cd Code\
+if "%1"=="-p" (
+  powershell -Command "& '%VSTestTool%' .\Test\Functional\bin\Release\Test.FunctionalCover.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings | Tee-Object -FilePath '%BATCH_HOME%..\ado_net_funtional_test.result'"
+) else (
+  call "%VSTestTool%" .\Test\Functional\bin\Release\Test.FunctionalCover.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
 )
